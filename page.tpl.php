b842365a55516f99311ba0cbac2473d10b0c94e2
Jim Zarkadas <dimitris.zarkadas@gmail.com>
2013-03-06 01:25:47 +0200

    Moves sidebar code to appropriate possition

--- page.tpl.php
+++ page.tpl.php
@@ -124,14 +124,14 @@
     <?php endif; ?>
 
     <?php print $messages; ?>
+    <?php if ($page['highlighted']): ?>
+      <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+    <?php endif; ?>
 
     <div id="main-wrapper">
       <div id="main" class="clearfix">
         <div id="content" class="column">
           <div class="section">
-            <?php if ($page['highlighted']): ?>
-              <div id="highlighted"><?php print render($page['highlighted']); ?></div>
-            <?php endif; ?>
             <a id="main-content"></a>
             <?php print render($title_prefix); ?>
             <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
